5fd4183cc59aff2b0a3e7db4a7d637a105a82052
Dhruv <dhruvanandsingh2004@gmail.com>
2024-01-15 18:21:55 +0530

    solved a minor error

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define:{
-    //defining environment varaibles
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY) 
-  }
+
 })
 
 // {import.meta.env.VITE_API_KEY}
