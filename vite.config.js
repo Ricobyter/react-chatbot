0335af8b1197fca2c25fce33da7f4f9a7b91dd38
Dhruv <dhruvanandsingh2004@gmail.com>
2024-01-15 18:48:24 +0530

    added some minor code

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
 })
 
-// {import.meta.env.VITE_API_KEY}
